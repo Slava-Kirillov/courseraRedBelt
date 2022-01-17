cb28f6e6d026274e2960daaa306590fe8a3031c9
vkirillov <viacheslav.kirillov@center2m.ru>
2022-01-17 16:55:56 +0300

    first commit

---
+++ final-task-part2/search_server.cpp
@@ -0,0 +1,121 @@
+#include "search_server.h"
+#include "iterator_range.h"
+#include "profile.h"
+
+#include <algorithm>
+#include <iterator>
+#include <sstream>
+#include <iostream>
+#include <array>
+#include <future>
+
+static const vector<pair<size_t, size_t>> empty_vec;
+
+vector<string_view> SplitIntoWordsView(string_view str) {
+    vector<string_view> result;
+    while (true) {
+        size_t space = str.find(' ');
+        string_view temp = str.substr(0, space);
+        if (!temp.empty()) {
+            result.push_back(temp);
+        }
+        if (space == std::string_view::npos) {
+            break;
+        } else {
+            str.remove_prefix(space + 1);
+        }
+    }
+    return move(result);
+}
+
+SearchServer::SearchServer(istream &document_input) {
+    UpdateDocumentBase(document_input);
+}
+
+void SearchServer::UpdateDocumentBase(istream &document_input) {
+    InvertedIndex new_index;
+
+    for (string current_document; getline(document_input, current_document);) {
+        new_index.Add(current_document);
+    }
+
+    index = move(new_index);
+}
+
+template<typename Container>
+void AddQueriesStreamThread(Container &queries, ostream &search_results_output, InvertedIndex &index) {
+    vector<pair<size_t, size_t>> search_results(index.getDocsCount(), make_pair(50001, 0));
+    for (auto &current_query: queries) {
+
+        vector<string_view> words = SplitIntoWordsView(current_query);
+        for (string_view word: words) {
+            for (pair<size_t, size_t> doc: index.Lookup(word)) {
+                search_results[doc.first].first = doc.first;
+                search_results[doc.first].second += doc.second;
+            }
+        }
+
+        partial_sort(
+                search_results.begin(),
+                Head(search_results, 5).end(),
+                search_results.end(),
+                [](pair<size_t, size_t> &lhs, pair<size_t, size_t> &rhs) {
+                    return (lhs.second > rhs.second) || (lhs.second == rhs.second && lhs.first < rhs.first);
+                }
+        );
+
+        search_results_output << current_query << ':';
+        for (auto &[docid, hitcount]: Head(search_results, 5)) {
+            if (hitcount > 0) {
+                search_results_output << " {docid: " << docid << ", hitcount: " << hitcount << '}';
+            }
+        }
+
+        search_results_output << endl;
+
+        search_results.assign(index.getDocsCount(), make_pair(50001, 0));
+    }
+}
+
+void SearchServer::AddQueriesStream(istream &query_input, ostream &search_results_output) {
+
+    vector<string> queries;
+
+    for (string current_query; getline(query_input, current_query);) {
+        queries.push_back(move(current_query));
+    }
+
+    size_t page_size = queries.size() / 4 != 0 ? queries.size() / 4 : queries.size();
+
+    vector<future<void>> futures;
+    auto &index_ = index;
+
+    for (auto page: Paginate(queries, page_size)) {
+        futures.push_back(
+                async([&page, &search_results_output, &index_] {
+                    AddQueriesStreamThread(page, search_results_output, index_);
+                })
+        );
+    }
+}
+
+void InvertedIndex::Add(string &document) {
+    docs.push_back(move(document));
+
+    for (string_view &word: SplitIntoWordsView(docs[docs.size() - 1])) {
+        vector<pair<size_t, size_t>> &vec = index[{word.begin(), word.end()}];
+        if (vec.empty() || vec.back().first != (docs.size() - 1)) {
+            vec.emplace_back(docs.size() - 1, 1);
+        } else {
+            vec.back().second++;
+        }
+    }
+}
+
+const vector<pair<size_t, size_t>> &InvertedIndex::Lookup(const string_view &word) const {
+    if (auto it = index.find(word); it != index.end()) {
+        return it->second;
+    } else {
+        return empty_vec;
+    }
+}
